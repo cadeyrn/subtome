adb190726519b697b30f748b8683fb90b90cee38
Sören Hentzschel <kontakt@agenedia.com>
2014-04-13 20:03:30 +0200

    version 2.0

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,27 +1,57 @@
+'use strict';
+
 const _ = require('sdk/l10n').get;
 const data = require('sdk/self').data;
 const panels = require('sdk/panel');
 const tabs = require('sdk/tabs');
-const widgets = require('sdk/widget');
 
-exports.main = function () {
-  const panel = panels.Panel({
-    height : 38,
-    contentURL : data.url('no-feed.html')
-  });
+const subtome = {
+  init : function () {
+    const message = panels.Panel({
+      height : 38,
+      contentURL : data.url('no-feed.html')
+    });
 
-  const widget = widgets.Widget({
-    id : 'subtome',
-    label : _('SUBSCRIBE_TO_PAGE'),
-    contentURL : data.url('toolbar-icon.png'),
-    onClick : function () {
-      var worker = tabs.activeTab.attach({
-        contentScriptFile : data.url('subtome.js')
-      });
-
-      worker.port.on('pageHasNoFeed', function () {
-        panel.show();
+    try {
+      require('sdk/ui/button/action').ActionButton({
+        id : 'subtome',
+        label : _('SUBSCRIBE_TO_PAGE'),
+        icon : {
+          '18' : data.url('icon-18.png'),
+          '32' : data.url('icon-32.png'),
+          '36' : data.url('icon-36.png'),
+          '64' : data.url('icon-64.png')
+        },
+        onClick : () => {
+          this.execute(message);
+        }
       });
     }
-  });
+    catch (e) {
+      require('sdk/widget').Widget({
+        id : 'copy-extensions',
+        label : _('SUBSCRIBE_TO_PAGE'),
+        contentURL : data.url('icon-32.png'),
+        onClick : () => {
+          this.execute(message);
+        }
+      });
+    }
+  },
+
+  execute : function (message) {
+    const worker = tabs.activeTab.attach({
+      contentScriptFile : data.url('subtome.js')
+    });
+
+    worker.port.on('pageHasNoFeed', () => {
+      message.show();
+    });
+  }
 };
+
+const main = () => {
+  subtome.init();
+};
+
+exports.main = main;
