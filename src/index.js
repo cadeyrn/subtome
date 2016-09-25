a5728aaa7a56d75a794a4f83d47bb4250600cbd3
Sören Hentzschel <kontakt@agenedia.com>
2016-09-25 11:11:42 +0200

    Unterstützung für Firefox kleiner als Version 29 entfernt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const _ = require('sdk/l10n').get;
+const actionbutton = require('sdk/ui/button/action');
 const data = require('sdk/self').data;
 const panels = require('sdk/panel');
 const tabs = require('sdk/tabs');
@@ -10,31 +11,19 @@ const subtome = {
       contentURL : data.url('no-feed.html')
     });
 
-    try {
-      require('sdk/ui/button/action').ActionButton({
-        id : 'subtome',
-        label : _('SUBSCRIBE_TO_PAGE'),
-        icon : {
-          '18' : data.url('icon-18.png'),
-          '32' : data.url('icon-32.png'),
-          '36' : data.url('icon-36.png'),
-          '64' : data.url('icon-64.png')
-        },
-        onClick : () => {
-          this.execute(message);
-        }
-      });
-    }
-    catch (e) {
-      require('sdk/widget').Widget({
-        id : 'copy-extensions',
-        label : _('SUBSCRIBE_TO_PAGE'),
-        contentURL : data.url('icon-32.png'),
-        onClick : () => {
-          this.execute(message);
-        }
-      });
-    }
+    actionbutton.ActionButton({
+      id : 'subtome',
+      label : _('SUBSCRIBE_TO_PAGE'),
+      icon : {
+        '18' : data.url('icon-18.png'),
+        '32' : data.url('icon-32.png'),
+        '36' : data.url('icon-36.png'),
+        '64' : data.url('icon-64.png')
+      },
+      onClick : () => {
+        this.execute(message);
+      }
+    });
   },
 
   execute : function (message) {
