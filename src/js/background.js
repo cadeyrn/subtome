0d6033470c33c23c39ade67a7d579fcc82514173
Sören Hentzschel <kontakt@agenedia.com>
2016-12-25 18:54:10 +0100

    aktiven Tab erhalten in eigene Methode ausgelagert

--- src/js/background.js
+++ src/js/background.js
@@ -1,21 +1,26 @@
 'use strict';
 
 const subtome = {
+  getActiveTab : function () {
+    return browser.tabs.query({ active : true, currentWindow : true });
+  },
+
   hasFeeds : function () {
-    browser.tabs.executeScript(null, { file : 'js/hasfeeds.js' });
+    subtome.getActiveTab().then((tabs) => {
+      let tabId = tabs[0].id;
+      browser.tabs.executeScript(null, { file : 'js/hasfeeds.js' });
+    });
   },
 
   handleResponse : function (response) {
-    let gettingActiveTab = browser.tabs.query({ active : true, currentWindow : true });
-
     if (response.message === 'has_no_feed') {
-      gettingActiveTab.then(function (tabs) {
+      subtome.getActiveTab().then((tabs) => {
         let tabId = tabs[0].id;
         browser.browserAction.disable(tabId);
         browser.browserAction.setTitle({ tabId : tabId, title : browser.i18n.getMessage('page_has_no_feed') });
       });
     } else {
-      gettingActiveTab.then(function (tabs) {
+      subtome.getActiveTab().then((tabs) => {
         let tabId = tabs[0].id;
         browser.browserAction.enable(tabId);
         browser.browserAction.setTitle({ tabId : tabId, title : browser.i18n.getMessage('actionbutton_title') });
