c3082799e6539a9366afddd9deb124762d76f89c
Sören Hentzschel <kontakt@agenedia.com>
2016-12-26 09:14:01 +0100

    Logik zum Aktivieren / Deaktivieren des Buttons in eigene Methoden ausgelagert

--- src/js/background.js
+++ src/js/background.js
@@ -5,23 +5,31 @@ const subtome = {
     return browser.tabs.query({ active : true, currentWindow : true });
   },
 
+  enableButton : function () {
+    subtome.getActiveTab().then((tabs) => {
+      let tabId = tabs[0].id;
+      browser.browserAction.enable(tabId);
+      browser.browserAction.setTitle({ tabId : tabId, title : browser.i18n.getMessage('actionbutton_title') });
+    });
+  },
+
+  disableButton : function () {
+    subtome.getActiveTab().then((tabs) => {
+      let tabId = tabs[0].id;
+      browser.browserAction.disable(tabId);
+      browser.browserAction.setTitle({ tabId : tabId, title : browser.i18n.getMessage('page_has_no_feed') });
+    });
+  },
+
   hasFeeds : function () {
     browser.tabs.executeScript(null, { file : 'js/hasfeeds.js', runAt : 'document_end' });
   },
 
   handleResponse : function (response) {
     if (response.message === 'has_no_feed') {
-      subtome.getActiveTab().then((tabs) => {
-        let tabId = tabs[0].id;
-        browser.browserAction.disable(tabId);
-        browser.browserAction.setTitle({ tabId : tabId, title : browser.i18n.getMessage('page_has_no_feed') });
-      });
+      subtome.disableButton();
     } else {
-      subtome.getActiveTab().then((tabs) => {
-        let tabId = tabs[0].id;
-        browser.browserAction.enable(tabId);
-        browser.browserAction.setTitle({ tabId : tabId, title : browser.i18n.getMessage('actionbutton_title') });
-      });
+      subtome.enableButton();
     }
   },
 
