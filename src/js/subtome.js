c5f5cfb83ba249e4750bedaf7a33ed01430d9d0e
Sören Hentzschel <kontakt@agenedia.com>
2021-12-23 12:28:18 +0100

    updated subtome.js based on changes on their side

--- src/js/subtome.js
+++ src/js/subtome.js
@@ -2,8 +2,11 @@
 
 var s = document.createElement('iframe');
 var resource = window.location.toString();
-s.setAttribute('style', 'position:fixed;top:0px; left:0px; width:100%; height:100%; border:0px; background: transparent; z-index: 2147483647');
-s.setAttribute('src', '//www.subtome.com/subscribe.html?resource=' + encodeURIComponent(resource) + '&feeds=' + feeds.join(','));
+var src = 'https://www.subtome.com';
+src += '/?subs';
+src += '/#/subscribe?resource=' + encodeURIComponent(resource) + '&feeds=' + encodeURIComponent(feeds.join(','));
+s.setAttribute('style', 'display:block; position:fixed; top:0px; left:0px; width:100%; height:100%; border:0px; background: transparent; z-index: 2147483647');
+s.setAttribute('src', src);
 var loaded = false;
 s.onload = function () {
   if (loaded) {
